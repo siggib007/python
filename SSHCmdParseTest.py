ebc0c1291bf36562b892b4a4737701c70add7011
Siggi Bjarnason (Work Laptop) <siggi.bjarnason@t-mobile.com>
2017-09-24 10:59:01 -0600

    SSHCmdparseTest

--- SSHCmdParseTest.py
+++ SSHCmdParseTest.py
@@ -13,32 +13,44 @@ DefUserName = getpass.getuser()
 print ("This is a SSH connect testing script. Your username is {3}. This is running under Python Version {0}.{1}.{2}".format(sys.version_info[0],sys.version_info[1],sys.version_info[2],DefUserName))
 now = time.asctime()
 print ("The time now is {}".format(now))
-strDevName  = getInput("Please provide hostname you wish to connect to: ")
+# strDevName  = getInput("Please provide hostname you wish to connect to: ")
+strDevName = "brcwsc31"
 strUserName = getInput("Please provide username for {0}, enter to use {1}: ".format(strDevName,DefUserName))
 if strUserName == "":
 	strUserName = DefUserName
 # end if username is empty
 strPWD = getpass.getpass(prompt="what is {0} password for {1}: ".format(strUserName,strDevName))
-strCommand  = getInput("Please provide command to run on {}: ".format(strDevName))
-print ("OK connecting to {0} as {1} and executing '{2}'".format(strDevName,strUserName,strCommand))
+# strCmd  = getInput("Please provide command to run on {}: ".format(strDevName))
+strCmd = "show bgp vrf INTERNET ipv6 unicast summary"
+print ("OK connecting to {0} as {1} and executing '{2}'".format(strDevName,strUserName,strCmd))
 try:
 	SSH = paramiko.SSHClient()
 	SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	SSH.connect(strDevName, username=strUserName, password=strPWD, look_for_keys=False, allow_agent=False, timeout=1)
-	stdin, stdout, stderr = SSH.exec_command(strCommand)
+	SSH.connect(strDevName, username=strUserName, password=strPWD, look_for_keys=False, allow_agent=False)
+	stdin, stdout, stderr = SSH.exec_command(strCmd)
+	print ("sent {0} to {1}".format(strCmd,strDevName))
 	strOut = stdout.read()
-	strErr = stderr.read()
-	print ("show command sent")
-	strErr = strErr.decode("utf-8")
+	SSH.close()
 	strOut = strOut.decode("utf-8")
-	print ("decoded error: {}".format(strErr))
 	print ("Command Output:\n{}".format(strOut))
 	SSH.close()
 except paramiko.ssh_exception.AuthenticationException as err:
 	print ("Auth Exception: {0}".format(err))
+	sys.exit(5)
 # except paramiko.SSHException as err:
 # 	print ("SSH Exception: {0}".format(err))
 # except OSError as err:
 # 	print ("socket Exception: {0}".format(err))
 # except Exception as err:
 # 	print ("Unknown error: {0}".format(err))
+strOutputLines = strOut.splitlines()
+i=0
+for strLine in strOutputLines:
+	print ("{} {}".format(i,strLine))
+	i+=1
+	if i>20:
+		strTokens = strLine.split()
+		itokencount = len(strTokens)
+		print ("There are {} tokens on this line".format(itokencount))
+		for token in strTokens:
+			print ("   - {}".format(token))
