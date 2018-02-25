6a5f41261a42c2aeb37f93c16852b0ea15c98211
Siggi Bjarnason (Work Laptop) <siggi.bjarnason@t-mobile.com>
2018-02-25 12:01:19 -0800

    Added ability to prompt for filename, instead of hard coding it in the script.

--- CSVFeed-IPCalc.py
+++ CSVFeed-IPCalc.py
@@ -9,13 +9,58 @@ pip install xmltodict
 pip install pymysql
 
 '''
-strFilein = "C:/temp/cmdb_ci_server.csv"
-strOutFile = "C:/temp/cmdb_ci_server-fixed.csv"
+# strFilein = "C:/temp/cmdb_ci_server.csv"
+# strOutFile = "C:/temp/cmdb_ci_server-fixed.csv"
 
 import sys
 import os
 import getpass
 import time
+try:
+	import tkinter as tk
+	from tkinter import filedialog
+	btKinterOK = True
+except:
+	print ("Failed to load tkinter, CLI only mode.")
+	btKinterOK = False
+
+def getInput(strPrompt):
+    if sys.version_info[0] > 2 :
+        return input(strPrompt)
+    else:
+        return raw_input(strPrompt)
+# end getInput
+
+strFilein = ""
+
+sa = sys.argv
+
+lsa = len(sys.argv)
+if lsa > 1:
+	strFilein = sa[1]
+
+if strFilein == "":
+	if btKinterOK:
+		print ("File name to be processed is missing. Opening up a file open dialog box, please select the file you wish to process.")
+		root = tk.Tk()
+		root.withdraw()
+		strFilein = filedialog.askopenfilename(title = "Select CSV file",filetypes = (("CSV files","*.csv"),("Text files","*.txt"),("all files","*.*")))
+	else:
+		strFilein = getInput("Please provide full path and filename for the CSV file to be processed: ")
+
+if strFilein == "":
+	print ("No filename provided unable to continue")
+	sys.exit()
+
+if os.path.isfile(strFilein):
+	print ("OK found {}".format(strFilein))
+else:
+	print ("Can't find CSV file {}".format(strFilein))
+	sys.exit(4)
+
+
+iLoc = strFilein.rfind(".")
+strOutFile = strFilein[:iLoc] + "-fixed" + strFilein[iLoc:]
 
 def DotDec2Int (strValue):
 	strHex = ""
