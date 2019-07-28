5d88bdaab9107c27bbf736c701de582d8f7854c0
SURFACEPROI5\Siggi <siggi@bjarnason.us>
2019-07-28 17:42:54 +0000

    fixes

--- json2CSV.py
+++ json2CSV.py
@@ -41,15 +41,24 @@ def ConvertJson2CSV(Itm2Bconverted):
 	if isinstance(Itm2Bconverted,dict):
 		for strKey in Itm2Bconverted.keys():
 			if isinstance(Itm2Bconverted[strKey],(list)):
-				Write2CSV(Itm2Bconverted[strKey])
+				Write2CSV(Itm2Bconverted[strKey],strKey)
 			else:
-				print ("{} is a {}".format(strKey,type(Itm2Bconverted[strKey])))
+				print ("{} is a {}, writing to CSV file as is.".format(strKey,type(Itm2Bconverted[strKey])))
+				objFileOut.write ("\n{},{}\n".format(strKey,Itm2Bconverted[strKey]))
 	elif isinstance(Itm2Bconverted,list):
-		Write2CSV(Itm2Bconverted)
+		Write2CSV(Itm2Bconverted,"")
 
-def Write2CSV(lstItem):
+def CSVClean (str2Clean):
+	strTemp = str(str2Clean)
+	strTemp = strTemp.replace(",","|")
+	return strTemp
+
+def Write2CSV(lstItem,strHeader):
 	strLine = ""
 	lstHeaders = []
+	if strHeader != "":
+		print ("Item is a section named {}".format(strHeader))
+	objFileOut.write("\n{}\n".format(strHeader))
 	print ("item is a {} and has {} elements".format(type(lstItem),len(lstItem)))
 	for outitem in lstItem:
 		for InnerItem in outitem.keys():
@@ -64,11 +73,20 @@ def Write2CSV(lstItem):
 		for strHeadKey in lstHeaders:
 			if strHeadKey in dictItem:
 				if isinstance(dictItem[strHeadKey],dict):
-					strLine += "dictionary with {} items,".format(len(dictItem[strHeadKey]))
+					if len(dictItem[strHeadKey]) == 1:
+						strLine += "{},".format(CSVClean(dictItem[strHeadKey]))
+					else:
+						strLine += "dictionary with {} items,".format(len(dictItem[strHeadKey]))
 				elif isinstance(dictItem[strHeadKey],list):
-					strLine += "list with {} items,".format(len(dictItem[strHeadKey]))
+					if len(dictItem[strHeadKey]) == 1:
+						if isinstance(dictItem[strHeadKey][0],(list,dict)):
+							strLine += "{} with {} elements,".format(type(dictItem[strHeadKey][0]),len(dictItem[strHeadKey][0]))
+						else:
+							strLine += "{},".format(CSVClean(dictItem[strHeadKey][0]))
+					else:
+						strLine += "list with {} items,".format(len(dictItem[strHeadKey]))
 				else:
-					strLine += str(dictItem[strHeadKey]) +","
+					strLine += "{},".format(CSVClean(dictItem[strHeadKey]))
 			else:
 				strLine += ","
 		strLine = strLine[:-1]
