8992885cb3052ac92bd12e9eb928ed9d2c9df04d
Sigurður G Bjarnason <sigurdur.gisli.bjarnason@advania.is>
2021-03-10 14:33:12 +0000

    wip

--- Bitsight.py
+++ Bitsight.py
@@ -17,12 +17,12 @@ import csv
 import socket
 
 try:
-	import tkinter as tk
-	from tkinter import filedialog
-	btKinterOK = True
+  import tkinter as tk
+  from tkinter import filedialog
+  btKinterOK = True
 except:
-	print ("Failed to load tkinter, CLI only mode.")
-	btKinterOK = False
+  print ("Failed to load tkinter, CLI only mode.")
+  btKinterOK = False
 # End imports
 
 #Default values, overwrite these in the ini file
@@ -35,98 +35,98 @@ def getInput(strPrompt):
         return raw_input(strPrompt)
 
 def LogEntry(strMsg):
-	print (strMsg)
+  print (strMsg)
 
 def SQLConn (strServer,strDBUser,strDBPWD,strInitialDB):
-	try:
-		# Open database connection
-		return pymysql.connect(host=strServer,user=strDBUser,password=strDBPWD,db=strInitialDB)
-	except pymysql.err.InternalError as err:
-		print ("Error: unable to connect: {}".format(err))
-		sys.exit(5)
-	except pymysql.err.OperationalError as err:
-		print ("Operational Error: unable to connect: {}".format(err))
-		sys.exit(5)
-	except pymysql.err.ProgrammingError as err:
-		print ("Programing Error: unable to connect: {}".format(err))
-		sys.exit(5)
+  try:
+    # Open database connection
+    return pymysql.connect(host=strServer,user=strDBUser,password=strDBPWD,db=strInitialDB)
+  except pymysql.err.InternalError as err:
+    print ("Error: unable to connect: {}".format(err))
+    sys.exit(5)
+  except pymysql.err.OperationalError as err:
+    print ("Operational Error: unable to connect: {}".format(err))
+    sys.exit(5)
+  except pymysql.err.ProgrammingError as err:
+    print ("Programing Error: unable to connect: {}".format(err))
+    sys.exit(5)
 
 def SQLQuery (strSQL,db):
-	try:
-		# prepare a cursor object using cursor() method
-		dbCursor = db.cursor()
-		# Execute the SQL command
-		dbCursor.execute(strSQL)
-		# Count rows
-		iRowCount = dbCursor.rowcount
-		if strSQL[:6].lower() == "select" or strSQL[:4].lower() == "call" or strSQL[:4].lower() == "show" or strSQL[:8].lower() == "describe":
-			dbResults = dbCursor.fetchall()
-		else:
-			db.commit()
-			dbResults = ()
-		return [iRowCount,dbResults]
-	except pymysql.err.InternalError as err:
-		if strSQL[:6].lower() != "select":
-			db.rollback()
-		return "Internal Error: unable to execute: {}\n{}".format(err,strSQL)
-	except pymysql.err.ProgrammingError as err:
-		if strSQL[:6].lower() != "select":
-			db.rollback()
-		return "Programing Error: unable to execute: {}\n{}".format(err,strSQL)
-	except pymysql.err.OperationalError as err:
-		if strSQL[:6].lower() != "select":
-			db.rollback()
-		return "Programing Error: unable to execute: {}\n{}".format(err,strSQL)
-	except pymysql.err.IntegrityError as err:
-		if strSQL[:6].lower() != "select":
-			db.rollback()
-		return "Integrity Error: unable to execute: {}\n{}".format(err,strSQL)
-	except Exception as err:
-		if strSQL[:6].lower() != "select":
-			db.rollback()
-		return "unknown Error: unable to execute: {}\n{}".format(err,strSQL)
+  try:
+    # prepare a cursor object using cursor() method
+    dbCursor = db.cursor()
+    # Execute the SQL command
+    dbCursor.execute(strSQL)
+    # Count rows
+    iRowCount = dbCursor.rowcount
+    if strSQL[:6].lower() == "select" or strSQL[:4].lower() == "call" or strSQL[:4].lower() == "show" or strSQL[:8].lower() == "describe":
+      dbResults = dbCursor.fetchall()
+    else:
+      db.commit()
+      dbResults = ()
+    return [iRowCount,dbResults]
+  except pymysql.err.InternalError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Internal Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.ProgrammingError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Programing Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.OperationalError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Programing Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.IntegrityError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Integrity Error: unable to execute: {}\n{}".format(err,strSQL)
+  except Exception as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "unknown Error: unable to execute: {}\n{}".format(err,strSQL)
 
 def DBClean(strText):
-	if strText.strip() == "":
-		return "NULL"
-	elif isInt(strText):
-		return strText #int(strText)
-	elif isFloat(strText):
-		return strText # float(strText)
-	else:
-		strTemp = strText.encode("ascii","ignore")
-		strTemp = strTemp.decode("ascii","ignore")
-		strTemp = strTemp.replace("\\","\\\\")
-		strTemp = strTemp.replace("'","\\'")
-		try:
-			strTemp = time.strftime("%Y-%m-%d",time.localtime(time.mktime(time.strptime(strTemp,strDTFormat))))
-		except ValueError:
-			pass
-		return "'" + strTemp + "'"
+  if strText.strip() == "":
+    return "NULL"
+  elif isInt(strText):
+    return strText #int(strText)
+  elif isFloat(strText):
+    return strText # float(strText)
+  else:
+    strTemp = strText.encode("ascii","ignore")
+    strTemp = strTemp.decode("ascii","ignore")
+    strTemp = strTemp.replace("\\","\\\\")
+    strTemp = strTemp.replace("'","\\'")
+    try:
+      strTemp = time.strftime("%Y-%m-%d",time.localtime(time.mktime(time.strptime(strTemp,strDTFormat))))
+    except ValueError:
+      pass
+    return "'" + strTemp + "'"
 
 def ValidReturn(lsttest):
-	if isinstance(lsttest,list):
-		if len(lsttest) == 2:
-			if isinstance(lsttest[0],int) and isinstance(lsttest[1],tuple):
-				return True
-			else:
-				return False
-		else:
-			return False
-	else:
-		return False
+  if isinstance(lsttest,list):
+    if len(lsttest) == 2:
+      if isinstance(lsttest[0],int) and isinstance(lsttest[1],tuple):
+        return True
+      else:
+        return False
+    else:
+      return False
+  else:
+    return False
 
 def isInt (CheckValue):
-	# function to safely check if a value can be interpreded as an int
-	if isinstance(CheckValue,int):
-		return True
-	elif isinstance(CheckValue,str):
-		if CheckValue.isnumeric():
-			return True
-		else:
-			return False
-	else:
-		return False
+  # function to safely check if a value can be interpreded as an int
+  if isinstance(CheckValue,int):
+    return True
+  elif isinstance(CheckValue,str):
+    if CheckValue.isnumeric():
+      return True
+    else:
+      return False
+  else:
+    return False
 
 def isFloat (fValue):
   if isinstance(fValue,(float,int,str)):
@@ -240,10 +240,10 @@ now = time.asctime()
 print ("The time now is {}".format(now))
 
 if os.path.isfile(strConf_File):
-	print ("Configuration File exists")
+  print ("Configuration File exists")
 else:
-	print ("Can't find configuration file {}, make sure it is the same directory as this script".format(strConf_File))
-	sys.exit(4)
+  print ("Can't find configuration file {}, make sure it is the same directory as this script".format(strConf_File))
+  sys.exit(4)
 
 strLine = "  "
 print ("Reading in configuration")
@@ -252,55 +252,55 @@ strLines = objINIFile.readlines()
 objINIFile.close()
 
 for strLine in strLines:
-	iCommentLoc = strLine.find("#")
-	if iCommentLoc > -1:
-		strLine = strLine[:iCommentLoc].strip()
-	else:
-		strLine = strLine.strip()
-	if "=" in strLine:
-		strConfParts = strLine.split("=")
-		strVarName = strConfParts[0].strip()
-		strValue = strConfParts[1].strip()
+  iCommentLoc = strLine.find("#")
+  if iCommentLoc > -1:
+    strLine = strLine[:iCommentLoc].strip()
+  else:
+    strLine = strLine.strip()
+  if "=" in strLine:
+    strConfParts = strLine.split("=")
+    strVarName = strConfParts[0].strip()
+    strValue = strConfParts[1].strip()
 
-		if strVarName == "Server":
-			strServer = strValue
-		if strVarName == "dbUser":
-			strDBUser = strValue
-		if strVarName == "dbPWD":
-			strDBPWD = strValue
-		if strVarName == "InitialDB":
-			strInitialDB = strValue
-		if strVarName == "FieldDelim":
-			strDelim = strValue
-		if strVarName == "CSVFileName":
-			strCSVName = strValue
-		if strVarName == "DateTimeFormat":
-			strDTFormat = strValue
+    if strVarName == "Server":
+      strServer = strValue
+    if strVarName == "dbUser":
+      strDBUser = strValue
+    if strVarName == "dbPWD":
+      strDBPWD = strValue
+    if strVarName == "InitialDB":
+      strInitialDB = strValue
+    if strVarName == "FieldDelim":
+      strDelim = strValue
+    if strVarName == "CSVFileName":
+      strCSVName = strValue
+    if strVarName == "DateTimeFormat":
+      strDTFormat = strValue
 
 sa = sys.argv
 
 lsa = len(sys.argv)
 if lsa > 1:
-	strCSVName = sa[1]
+  strCSVName = sa[1]
 
 if strCSVName == "":
-	if btKinterOK:
-		print ("File name to be imported is missing. Opening up a file open dialog box, please select the file you wish to import.")
-		root = tk.Tk()
-		root.withdraw()
-		strCSVName = filedialog.askopenfilename(title = "Select CSV file",filetypes = (("CSV files","*.csv"),("Text files","*.txt"),("all files","*.*")))
-	else:
-		strCSVName = getInput("Please provide full path and filename for the CSV file to be imported: ")
+  if btKinterOK:
+    print ("File name to be imported is missing. Opening up a file open dialog box, please select the file you wish to import.")
+    root = tk.Tk()
+    root.withdraw()
+    strCSVName = filedialog.askopenfilename(title = "Select CSV file",filetypes = (("CSV files","*.csv"),("Text files","*.txt"),("all files","*.*")))
+  else:
+    strCSVName = getInput("Please provide full path and filename for the CSV file to be imported: ")
 
 if strCSVName == "":
-	print ("No filename provided unable to continue")
-	sys.exit()
+  print ("No filename provided unable to continue")
+  sys.exit()
 
 if os.path.isfile(strCSVName):
-	print ("OK found {}".format(strCSVName))
+  print ("OK found {}".format(strCSVName))
 else:
-	print ("Can't find CSV file {}".format(strCSVName))
-	sys.exit(4)
+  print ("Can't find CSV file {}".format(strCSVName))
+  sys.exit(4)
 
 dbConn = SQLConn (strServer,strDBUser,strDBPWD,strInitialDB)
 LogEntry("Starting the import of {} into database on {}".format(strCSVName,strServer))
@@ -309,43 +309,72 @@ LogEntry ("Truncating exiting table")
 strSQL = "delete from tblbitsightvulns;"
 lstReturn = SQLQuery (strSQL,dbConn)
 if not ValidReturn(lstReturn):
-	print ("Unexpected: {}".format(lstReturn))
-	sys.exit(9)
+  print ("Unexpected: {}".format(lstReturn))
+  sys.exit(9)
 else:
-	LogEntry ("Deleted {} old records".format(lstReturn[0]))
+  LogEntry ("Deleted {} old records".format(lstReturn[0]))
 
 iLine = 0
 with open(strCSVName,newline="") as hCSV:
-	myReader = csv.reader(hCSV, delimiter=strDelim)
-	lstLine = next(myReader)
-	LogEntry ("Starting import...")
+  myReader = csv.reader(hCSV, delimiter=strDelim)
+  lstLine = next(myReader)
+  LogEntry ("Starting import...")
 
-	for lstLine in myReader :
-		if lstLine[0][:10] == "Disclaimer" or lstLine[0][:11] == "*IP address":
-			continue 
-		lstFindingParts = lstLine[1].split(":")
-		if ValidateIP(lstFindingParts[0]):
-			strIPAddress = "'{}'".format(lstFindingParts[0])
-		else:
-			try:
-				strDNS = socket.gethostbyname(lstFindingParts[0])
-			except Exception as err:
-				strDNS = err
-			strIPAddress = "'{}'".format(strDNS)
-		lstValues = []
-		iLine += 1
-		for strCSV in lstLine:
-			lstValues.append(DBClean(strCSV))
-		lstValues.append(strIPAddress)
-		strSQL = ("insert into tblbitsightvulns (vcRiskVector,vcFindingID,dtFirstSeen," 
-							"dtLastSeen,vcGrade,vcImpactRVG,iLifeTime,vcSeverity,vcDetails,iSrcPort,"
-							"iDstPort,vcPort,vcSrvType,vcSrvVer,vcRefresh,vcIPAddr) "
-							" values ({});".format(",".join(lstValues)))
-		lstReturn = SQLQuery (strSQL,dbConn)
-		if not ValidReturn(lstReturn):
-			print ("Unexpected: {}".format(lstReturn))
-			sys.exit(9)
-		else:
-			print ("Inserted {} record".format(lstReturn[0]),end="\r")
+  for lstLine in myReader :
+    if lstLine[0][:10] == "Disclaimer" or lstLine[0][:11] == "*IP address":
+      continue 
+    lstFindingParts = lstLine[1].split(":")
+    if ValidateIP(lstFindingParts[0]):
+      strIPAddress = lstFindingParts[0]
+    else:
+      try:
+        strDNS = socket.gethostbyname(lstFindingParts[0])
+      except Exception as err:
+        strDNS = err
+      strIPAddress = strDNS
+    
+    lstValues = []
+    lstCustomer = []
+    lstDescr = []
+    if strIPAddress[0] != "[":
+      dictIPInfo = IPCalc(strIPAddress)
+      strSQL = ("SELECT vcCustomer,vcDescription,iBitMask FROM tbl_ipam"
+                " WHERE iNetID <= {0} AND iBroadcast >= {0} "
+                " ORDER BY iHostCount;".format(dictIPInfo["DecIP"]))
+      lstReturn = SQLQuery (strSQL,dbConn)
+      if not ValidReturn(lstReturn):
+        print ("Unexpected: {}".format(lstReturn))
+        sys.exit(9)
+      else:
+        for dbRow in lstSubnets[1]:
+          strCustomer = dbRow[0]
+          strDescription = dbRow[1]
+          iBitMask = int(dbRow[2])
+          if iBitMask > 22:
+            if strCustomer != "":
+              lstCustomer.append(strCustomer)
+            if strDescription != "":
+              lstDescr.append(strDescription)
+          if iBitMask = 32:
+            break
+    strCustomer = ";".join(lstCustomer)
+    strDescription = ";".join(lstDescr)
+
+    iLine += 1
+    for strCSV in lstLine:
+      lstValues.append(DBClean(strCSV))
+    lstValues.append( "'{}'".format(strIPAddress))
+    lstValues.append( "'{}'".format(strCustomer))
+    lstValues.append( "'{}'".format(strDescription))
+    strSQL = ("insert into tblbitsightvulns (vcRiskVector,vcFindingID,dtFirstSeen," 
+              "dtLastSeen,vcGrade,vcImpactRVG,iLifeTime,vcSeverity,vcDetails,iSrcPort,"
+              "iDstPort,vcPort,vcSrvType,vcSrvVer,vcRefresh,vcIPAddr,vcCustomer,vcNetDescr) "
+              " values ({});".format(",".join(lstValues)))
+    lstReturn = SQLQuery (strSQL,dbConn)
+    if not ValidReturn(lstReturn):
+      print ("Unexpected: {}".format(lstReturn))
+      sys.exit(9)
+    else:
+      print ("Inserted {} record".format(lstReturn[0]),end="\r")
 LogEntry("Done. Processed {} records".format(iLine))
 
